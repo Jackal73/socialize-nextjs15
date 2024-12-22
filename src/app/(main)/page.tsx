175975d5b1dbf9a52914c806638aef009fb17ec8
Shawn Kebel <fxrender@outlook.com>
2024-12-21 21:31:53 -0600

    :sparkles::children_crossing: `feed` -  Implement `FollowingFeed` component with `infinite scroll` for posts from followed users; add `API route` for `fetching posts` and `integrate with tabs` in the home page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,22 +2,28 @@ import PostEditor from "@/components/posts/editor/PostEditor";
 import Post from "@/components/posts/Post";
 import TrendsSidebar from "@/components/TrendsSidebar";
 import prisma from "@/lib/prisma";
-import { postDataInclude } from "@/lib/types";
+
 import ForYouFeed from "./ForYouFeed";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import FollowingFeed from "./FollowingFeed";
 
 export default function Home() {
-  // const posts = await prisma.post.findMany({
-  //   include: postDataInclude,
-  //   orderBy: { createdAt: "desc" },
-  // });
   return (
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
-        {/* {posts.map((post) => (
-          <Post key={post.id} post={post} />
-        ))} */}
-        <ForYouFeed />
+        <Tabs>
+          <TabsList>
+            <TabsTrigger value="for-you">For you</TabsTrigger>
+            <TabsTrigger value="following">Following</TabsTrigger>
+          </TabsList>
+          <TabsContent value="for-you">
+            <ForYouFeed />
+          </TabsContent>
+          <TabsContent value="following">
+            <FollowingFeed />
+          </TabsContent>
+        </Tabs>
       </div>
       <TrendsSidebar />
     </main>
