26a88acdc7c48897b92abaa10c1f30ec8caeda1d
Shawn Kebel <fxrender@outlook.com>
2024-12-24 00:44:16 -0600

    :construction: `Like` - Add GET route for retrieving `likes information` of a post with `user validation`

---
+++ src/app/api/users/[userId]/posts/[postId]/likes/route.ts
@@ -0,0 +1,49 @@
+import { validateRequest } from "@/auth";
+import prisma from "@/lib/prisma";
+import { LikeInfo } from "@/lib/types";
+
+export async function GET(
+  req: Request,
+  { params: { postId } }: { params: { postId: string } },
+) {
+  try {
+    const { user: loggedInUser } = await validateRequest();
+
+    if (!loggedInUser) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const post = await prisma.post.findUnique({
+      where: { id: postId },
+      select: {
+        likes: {
+          where: {
+            userId: loggedInUser.id,
+          },
+          select: {
+            userId: true,
+          },
+        },
+        _count: {
+          select: {
+            likes: true,
+          },
+        },
+      },
+    });
+
+    if (!post) {
+      return Response.json({ error: "Post not found" }, { status: 404 });
+    }
+
+    const data: LikeInfo = {
+      likes: post._count.likes,
+      isLikedByUser: !!post.likes.length,
+    };
+
+    return Response.json(data);
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
