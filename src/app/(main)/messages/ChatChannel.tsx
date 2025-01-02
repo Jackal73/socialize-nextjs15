25f52a3769594a946c9ce41917c2c4d960b135ee
Shawn Kebel <fxrender@outlook.com>
2025-01-02 01:03:17 -0600

    :children_crossing::sparkles: `chatEnhancements` - Refactor chat components to support `sidebar toggle`; implement user search functionality in `NewChatDialog` with debouncing; add `custom channel header` and `user selection` features

--- src/app/(main)/messages/ChatChannel.tsx
+++ src/app/(main)/messages/ChatChannel.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Menu } from "lucide-react";
 import {
   Channel,
   ChannelHeader,
@@ -7,17 +10,17 @@ import {
   Window,
 } from "stream-chat-react";
 
-// interface ChatChannelProps {
-//   open: boolean;
-//   openSidebar: () => void;
-// }
+interface ChatChannelProps {
+  open: boolean;
+  openSidebar: () => void;
+}
 
-export default function ChatChannel() {
+export default function ChatChannel({ open, openSidebar }: ChatChannelProps) {
   return (
-    <div className="w-full">
+    <div className={cn("w-full md:block", !open && "hidden")}>
       <Channel>
         <Window>
-          <ChannelHeader />
+          <CustomChannelHeader openSidebar={openSidebar} />
           <MessageList />
           <MessageInput />
         </Window>
@@ -33,4 +36,15 @@ interface CustomChannelHeaderProps extends ChannelHeaderProps {
 function CustomChannelHeader({
   openSidebar,
   ...props
-}: CustomChannelHeaderProps) {}
+}: CustomChannelHeaderProps) {
+  return (
+    <div className="flex items-center gap-3">
+      <div className="h-full p-2 md:hidden">
+        <Button size="icon" variant="ghost" onClick={openSidebar}>
+          <Menu className="size-5" />
+        </Button>
+      </div>
+      <ChannelHeader />
+    </div>
+  );
+}
--- src/app/(main)/messages/ChatSidebar.tsx
+++ src/app/(main)/messages/ChatSidebar.tsx
@@ -1,16 +1,44 @@
-import { ChannelList } from "stream-chat-react";
+import {
+  ChannelList,
+  ChannelPreviewMessenger,
+  ChannelPreviewUIComponentProps,
+} from "stream-chat-react";
 import { useSession } from "../SessionProvider";
+import { Button } from "@/components/ui/button";
+import { MailPlus, X } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { useCallback, useState } from "react";
+import NewChatDialog from "./NewChatDialog";
 
-// interface ChatSidebarProps {
-//   open: boolean;
-//   onClose: () => void;
-// }
+interface ChatSidebarProps {
+  open: boolean;
+  onClose: () => void;
+}
 
-export default function ChatSidebar() {
+export default function ChatSidebar({ open, onClose }: ChatSidebarProps) {
   const { user } = useSession();
 
+  const ChannelPreviewCustom = useCallback(
+    (props: ChannelPreviewUIComponentProps) => (
+      <ChannelPreviewMessenger
+        {...props}
+        onSelect={() => {
+          props.setActiveChannel?.(props.channel, props.watchers);
+          onClose();
+        }}
+      />
+    ),
+    [onClose],
+  );
+
   return (
-    <div className="size-full flex-col border-e md:flex md:w-72">
+    <div
+      className={cn(
+        "size-full flex-col border-e md:flex md:w-72",
+        open ? "flex" : "hidden",
+      )}
+    >
+      <MenuHeader onClose={onClose} />
       <ChannelList
         filters={{
           type: "messaging",
@@ -27,7 +55,46 @@ export default function ChatSidebar() {
             },
           },
         }}
+        Preview={ChannelPreviewCustom}
       />
     </div>
+  );
+}
+
+interface MenuHeaderProps {
+  onClose: () => void;
+}
+
+function MenuHeader({ onClose }: MenuHeaderProps) {
+  const [showNewChatDialog, setShowNewChatDialog] = useState(false);
+
+  return (
+    <>
+      <div className="flex items-center gap-3 p-2">
+        <div className="h-full md:hidden">
+          <Button size="icon" variant="ghost" onClick={onClose}>
+            <X className="size-5" />
+          </Button>
+        </div>
+        <h1 className="me-auto text-xl font-bold md:ms-2">Messages</h1>
+        <Button
+          size="icon"
+          variant="ghost"
+          title="Start new chat"
+          onClick={() => setShowNewChatDialog(true)}
+        >
+          <MailPlus className="size-5" />
+        </Button>
+      </div>
+      {showNewChatDialog && (
+        <NewChatDialog
+          onOpenChange={setShowNewChatDialog}
+          onChatCreated={() => {
+            setShowNewChatDialog(false);
+            onClose();
+          }}
+        />
+      )}
+    </>
   );
 }
